8078151bd35783e9f549b9dd767a388541d69516
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-09-13 14:19:58 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -6,11 +6,11 @@ class Artist < ActiveRecord::Base
   # def slug
   #   name.downcase.gsub(" ","-")
   # end
-  # 
+  #
   # def self.find_by_slug(slug)
   #   Artist.all.find{|artist| artist.slug == slug}
   # end
 
   InstanceMethods.slug
-  
+
 end
